e402738f8638c8432f1bfd3fb58e5a7a568129ec
tamburkey <petem14@gmail.com>
2020-07-31 23:57:52 -0400

    Updates

---
+++ precache-manifest.abacd37ab3f92fe0429144cf12eb1d4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f93a09b697945d071303d86ef1e662f",
+    "url": "/test-gh-pages-cra-2/index.html"
+  },
+  {
+    "revision": "33d4c29c7e1f8c98851b",
+    "url": "/test-gh-pages-cra-2/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "760960c4001174e05c1f",
+    "url": "/test-gh-pages-cra-2/static/js/2.3b004fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-pages-cra-2/static/js/2.3b004fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d4c29c7e1f8c98851b",
+    "url": "/test-gh-pages-cra-2/static/js/main.441cebce.chunk.js"
+  },
+  {
+    "revision": "e20bbed9e965dc9e6cdc",
+    "url": "/test-gh-pages-cra-2/static/js/runtime-main.8580381f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/test-gh-pages-cra-2/static/media/logo.ee7cd8ed.svg"
+  }
+]);
